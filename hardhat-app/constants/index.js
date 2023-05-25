32ab9ffeb4b2c04640c68ef517fc2fe4ff4c9955
Pavel Stelmakh <pavel_stelmakh@epam.com>
2023-05-26 00:03:46 +0300

    updated contract address

--- hardhat-app/constants/index.js
+++ hardhat-app/constants/index.js
@@ -1,6 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = '0xc2471FD1b52Db7F006bb321Cbd19449FB04e3411';
 
-const METADATA_URL = 'https://localhost:3000/api/';
+const METADATA_URL = 'https://learn-web3-nft-collection-one.vercel.app/api/';
 
 module.exports = {
     WHITELIST_CONTRACT_ADDRESS,
